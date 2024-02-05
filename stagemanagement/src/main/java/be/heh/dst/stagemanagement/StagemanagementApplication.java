50c2926c0df710bed9bb678c9c45fab0a27298a7
honore <luishonore0511@gmail.com>
2024-02-05 14:39:30 +0100

    Premier commit

---
+++ stagemanagement/src/main/java/be/heh/dst/stagemanagement/StagemanagementApplication.java
@@ -0,0 +1,13 @@
+package be.heh.dst.stagemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StagemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StagemanagementApplication.class, args);
+	}
+
+}
---
+++ stagemanagement/src/test/java/be/heh/dst/stagemanagement/StagemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package be.heh.dst.stagemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StagemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
