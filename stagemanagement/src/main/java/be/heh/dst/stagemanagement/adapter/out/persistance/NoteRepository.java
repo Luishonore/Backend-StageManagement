bd712a20f98375a6554ed5a7306d75d7567d28b7
honore <luishonore0511@gmail.com>
2024-03-14 13:57:25 +0100

    [0.1.0]
    feat:
    - fonction d'ajout, suppression, recherche par nom et récupération de la liste des sociétés dans la base de données,
    - fonction d'ajout, suppression, recherche par ID et récupération de la liste des notes dans la base de données,
    - création des tests unitaire des nouvelles fonctionnalitées ajoutées,
    - création d'un système de Logs.

--- stagemanagement/src/main/java/be/heh/dst/stagemanagement/adapter/out/persistance/NoteRepository.java
+++ stagemanagement/src/main/java/be/heh/dst/stagemanagement/adapter/out/persistance/NoteRepository.java
@@ -4,8 +4,6 @@ import be.heh.dst.stagemanagement.application.domain.model.Note;
 import be.heh.dst.stagemanagement.application.port.out.NotePortOut;
 
 import lombok.extern.log4j.Log4j2;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
